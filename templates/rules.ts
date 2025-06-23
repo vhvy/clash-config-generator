4a528f412804d222bc1fa41fd99f94ef4ebf575e
vhvy <vhvy@qq.com>
2025-06-24 00:06:33 +0800

    feat: Remove hod rules

--- templates/rules.ts
+++ templates/rules.ts
@@ -60,7 +60,6 @@ export const ruleProviders = {
 };
 
 export const rules = [
-  `${RuleTypeEnum.DOMAIN},hod.danswer.me,${ProxyTypeEnum.Direct}`,
   `${RuleTypeEnum.DOMAIN_SUFFIX},docker.com,${ProxyTypeEnum.Overseas}`,
   `${RuleTypeEnum.DOMAIN_SUFFIX},archive.org,${ProxyTypeEnum.Overseas}`,
   `${RuleTypeEnum.DOMAIN_KEYWORD},cyc-anime,${RuleActionEnum.DIRECT}`,
